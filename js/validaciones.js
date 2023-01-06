687dba836bd287aabfb602b352088fa4a3e593e1
Vanessa <110750471+nattdev@users.noreply.github.com>
2023-01-06 08:54:11 -0500

    Clase03 Completada

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,12 +1,26 @@
-const todayDate = new Date();
-const inputBirth = document.querySelector("#birth");
+export function valida(input) {
+	const tipoDeInput = input.dataset.tipo;
+	console.log(tipoDeInput);
+	console.log("typeof", typeof(tipoDeInput));
+	if(validadores[tipoDeInput]) {
+		validadores[tipoDeInput](input);
+	}
+}
 
-inputBirth.addEventListener("blur", validarDate); 
+const validadores = {
+	nacimiento: (input) => validarDate(),
+}
+
+
+//const todayDate = new Date();
+//const inputBirth = document.querySelector("#birth");
+
+//inputBirth.addEventListener("blur", validarDate); 
 
 function validarDate() {
 	const userDate = new Date(inputBirth.value + "T00:00:00");
 	let mensaje = "";
-	if(!mayorDeEdad(userDate)){
+	if (!mayorDeEdad(userDate)) {
 		mensaje = "Debe tener al menos 18 años de edad";
 	} 
 	inputBirth.setCustomValidity(mensaje);
